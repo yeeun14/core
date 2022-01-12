cbe164a24b84a8d8567a234fc9fe7e0f51b6ae30
yeeun14 <yeaeen0214@naver.com>
2022-01-12 19:17:07 +0900

    2022-01-12

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService { // 주문 생성 요청
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책에 멤버정보 전달
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
