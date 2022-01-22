4d0a7072b089ddb619f7c6a0a1b1b1726589ea2e
yeeun14 <yeaeen0214@naver.com>
2022-01-22 23:58:36 +0900

    2022-01-22 1. 스프링 빈 설정 2. 싱글톤 패턴

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig { // 구현객체 생성
 
+    //@Bean MemberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 생서자를 통해서 객체가 들어간다고 해, 생성자 주입이라고 함
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -27,4 +27,9 @@ public class OrderServiceImpl implements OrderService { // 주문 생성 요청
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    //테스트 용도도
+   public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
